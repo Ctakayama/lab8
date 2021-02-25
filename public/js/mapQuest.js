9fff6585922371c24c46542fd8a3cf8f0397b684
ctakayama <cole.takayama@gmail.com>
2021-02-25 10:08:19 -0800

    third party API lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+
+	L.mapquest.key = 'FX6RsgBLwAEOyLlkA7BKinfmNlX98Hbo';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	center: [32.87960966358866, -117.23635512680602],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+	});
+
+	L.marker([32.87960966358866, -117.23635512680602]).addTo(map);
+
 }
